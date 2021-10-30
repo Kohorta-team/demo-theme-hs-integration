b5fd2ffe018a25504f73ef34207ae1c32c3f224d
Roman Kozak <roman.gruit@gmail.com>
2021-10-30 13:42:36 +0300

    improved understadability of code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # demo-theme-hs-integration
+
+To start type in terminal:
+
+1) npm i
+2) npm start
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const FormData = require("form-data");
 const path = require("path");
 const fs = require("fs");
 
+// value of AUTHORIZATION_URL has been copied from HubSpot app creation page in developer account
 const AUTHORIZATION_URL = `https://app.hubspot.com/oauth/authorize?client_id=49985d26-b5f2-4d4f-8f8f-41d2416de4ac&redirect_uri=http://localhost:3000/authenticated&scope=contacts%20content%20oauth`;
 
 const app = express();
@@ -14,6 +15,7 @@ app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
+// values of client_id, client_secret and redirect_uri have been copied from HubSpot app creation page in developer account
 const formData = {
   grant_type: "authorization_code",
   client_id: "49985d26-b5f2-4d4f-8f8f-41d2416de4ac",
@@ -22,23 +24,24 @@ const formData = {
   code: "",
 };
 
-const postAuthOptions = {
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-};
-
 app.get("/authenticated", async (req, res) => {
+  //adding authorization code to object which will be exchanged to access token via POST request
   formData.code = req.query.code;
   const stringData = querystring.stringify(formData);
 
+  //Exchanging authorization code for tokens via POST request
   try {
     const tokenRes = await axios.post(
       "https://api.hubapi.com/oauth/v1/token",
       stringData,
-      postAuthOptions
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
     );
 
+    //if successful json object with access token will be returned
     const access_token = tokenRes.data.access_token;
     console.log({ access_token });
     const form = new FormData();
@@ -48,6 +51,7 @@ app.get("/authenticated", async (req, res) => {
     );
     const formHeaders = form.getHeaders();
 
+    //be careful to upload an archive with already existed file name at user's portal
     const fileName = `${Date.now()}_demo_theme.zip`;
 
     const themeResp = await axios.post(
@@ -68,7 +72,7 @@ app.get("/authenticated", async (req, res) => {
       {
         headers: {
           Authorization: `Bearer ${access_token}`,
-          accept: "application/json",
+          "Content-Type": "application/json",
         },
       }
     );
@@ -79,12 +83,12 @@ app.get("/authenticated", async (req, res) => {
         name: "Home demo page",
         template_path: "demo_theme/templates/home.html",
         publish_immediately: true,
-        slug: "demo-homepage",
+        slug: `${Date.now()}-demo-homepage`,
         is_draft: false,
         subcategory: "site_page",
-        html_title: "tttttt2",
-        page_title: "tttttt2",
-        title: "tttttt2",
+        html_title: "Home demo page",
+        page_title: "Home demo page",
+        title: "Home demo page",
         is_published: true,
         state: "PUBLISHED_OR_SCHEDULED",
         current_state: "PUBLISHED",
@@ -96,9 +100,14 @@ app.get("/authenticated", async (req, res) => {
         },
       }
     );
+    const pageUrl = pageResp.data.url;
+    console.log(pageUrl);
 
-    console.log(pageResp.data);
-    console.log(pageResp.data.url);
+    let respFile = fs.readFileSync(path.join(__dirname, "views/auth.html"), {
+      encoding: "utf-8",
+    });
+    respFile = pageUrl ? respFile.replace("#LINK", pageUrl) : respFile;
+    res.send(respFile);
   } catch (error) {
     console.log("Error in uploadTemplate");
     if (error.isAxiosError) {
@@ -108,7 +117,6 @@ app.get("/authenticated", async (req, res) => {
       console.log(error);
     }
   }
-  res.sendFile(path.join(__dirname, "views/auth.html"));
 });
 
 app.use((error, req, res, next) => {
